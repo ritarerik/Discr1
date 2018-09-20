f88b20c73c025ebb2c223f6a7d6d043fd85f5218
metelevamd <metelevamd@KM-DIT-N01.doronichi.com>
2018-09-20 14:55:46 +0300

    5

---
+++ src/RandomMatrix.java
@@ -0,0 +1,25 @@
+
+// создание матрицы с рандомными элементами 
+
+public class RandomMatrix {
+	
+	public static int[][] createINT(int size) {
+		
+		int A[][] = new int[size][size];
+		
+		
+		return A;
+		
+	}
+	
+	public static boolean[][] createBOOLEAN(int size) {
+		
+		boolean A[][] = new boolean[size][size];
+		
+		
+		return A;
+		
+	}
+	
+	
+}
