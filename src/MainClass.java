a477a25450862e06ee59190342ec3c68e25a2d18
metelevamd <metelevamd@KM-DIT-N01.doronichi.com>
2018-09-20 15:17:06 +0300

    6

--- src/MainClass.java
+++ src/MainClass.java
@@ -10,16 +10,15 @@ public class MainClass {
 			
 			System.out.println("1 > пересечение графов");
 			System.out.println("2 > минимальная полустепень захода");
-			System.out.println("3 > выход");
+			System.out.println("3 > случайная матрица INT");
+			System.out.println("4 > случайная матрица BOOLEAN");
+			System.out.println("5 > выход");
 			System.out.println(">> ");
 			
 			Scanner in = new Scanner(System.in);			
 			int num = 0;
 			String res = "";
-			try { num = in.nextInt();		        
-	        } catch (Exception e) {
-	        	continue;
-	        }
+			try { num = in.nextInt(); } catch (Exception e) { continue; }
 			
 			 switch(num) {     
 	        	case 1:
@@ -49,6 +48,36 @@ public class MainClass {
 	        		for (int i = 0; i < D.length; i ++) {
 	        			System.out.print(E[i]);	        			
 	        			if (i < D.length - 1) System.out.print(" ");	        			
+	        		}
+	        		
+	        		break;
+	        		
+	        	case 3 :
+	        		
+	        		int F[][] = RandomMatrix.createINT(10);
+	        		
+	        		for (int i = 0; i < F.length; i ++) {
+	        			for (int j = 0; j < F.length; j++) {
+	        				System.out.print(F[i][j]);	        				
+	        				if (j < F.length - 1) System.out.print(" ");	        				
+	        			}
+	        			
+	        			if (i < F.length - 1) System.out.print("\n");	        			
+	        		}
+	        		
+	        		break;
+	        	
+	        	case 4:
+	        		
+	        		boolean G[][] = RandomMatrix.createBOOLEAN(10);
+	        		
+	        		for (int i = 0; i < G.length; i ++) {
+	        			for (int j = 0; j < G.length; j++) {
+	        				System.out.print(G[i][j]);	        				
+	        				if (j < G.length - 1) System.out.print(" ");	        				
+	        			}
+	        			
+	        			if (i < G.length - 1) System.out.print("\n");	        			
 	        		}
 	        		
 	        		break;
--- src/RandomMatrix.java
+++ src/RandomMatrix.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 
 // создание матрицы с рандомными элементами 
 
@@ -7,6 +8,11 @@ public class RandomMatrix {
 		
 		int A[][] = new int[size][size];
 		
+		Random rnd = new Random(System.currentTimeMillis());
+		
+		for (int i = 0; i < size; i++)
+			for (int j = 0; j < size; j++)
+				A[i][j] = rnd.nextInt(10);
 		
 		return A;
 		
@@ -16,6 +22,11 @@ public class RandomMatrix {
 		
 		boolean A[][] = new boolean[size][size];
 		
+		Random rnd = new Random(System.currentTimeMillis());
+		
+		for (int i = 0; i < size; i++)
+			for (int j = 0; j < size; j++)
+				A[i][j] = rnd.nextBoolean();
 		
 		return A;
 		
